791979cc9af830585e61e054859087e433d10fe4
profesone <profesone@gmail.com>
2024-02-06 20:02:31 -0600

    Client portal completed.

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full mx-auto items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
         <a class="text-white text-sm uppercase hidden lg:inline-block font-semibold" href="#">
             {{-- Dashboard --}}
-            Hello {{ Auth::user()->name }}
+            Hello {{ auth()->user()->name }}
         </a>
 
         {{-- If you use user icon and menu add margin mr-3 to search --}}
