36063d52b9d104459ef9ebcbb70cbd5bbbc8df3c
profesone <profesone@gmail.com>
2024-11-13 08:08:49 -0600

    Completed Client Portal.

--- resources/views/admin/client/index.blade.php
+++ resources/views/admin/client/index.blade.php
@@ -8,11 +8,13 @@
                     {{ trans('cruds.client.title_singular') }}
                     {{ trans('global.list') }}
                 </h6>
-
                 @can('client_create')
+                    @if(auth()->user()->roles[0]->id != 3)
                     <a class="btn btn-indigo" href="{{ route('admin.clients.create') }}">
+
                         {{ trans('global.add') }} {{ trans('cruds.client.title_singular') }}
                     </a>
+                    @endif
                 @endcan
             </div>
         </div>
