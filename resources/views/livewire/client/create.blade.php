70e88c46e934388731094070176855709cfc6c87
profesone <profesone@gmail.com>
2024-11-13 08:55:13 -0600

    Fixed CreatClient for Admins.

--- resources/views/livewire/client/create.blade.php
+++ resources/views/livewire/client/create.blade.php
@@ -117,136 +117,150 @@
             {{ trans('cruds.client.fields.email_address_response_helper') }}
         </div>
     </div>
-{{--    <div class="form-group {{ $errors->has('client.default_messages_module') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="default_messages_module" id="default_messages_module" wire:model.defer="client.default_messages_module">--}}
-{{--        <label class="form-label inline ml-1" for="default_messages_module">{{ trans('cruds.client.fields.default_messages_module') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.default_messages_module') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.default_messages_module_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.default_message_notification') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="default_message_notification" id="default_message_notification" wire:model.defer="client.default_message_notification">--}}
-{{--        <label class="form-label inline ml-1" for="default_message_notification">{{ trans('cruds.client.fields.default_message_notification') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.default_message_notification') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.default_message_notification_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.default_message_response') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="default_message_response" id="default_message_response" wire:model.defer="client.default_message_response">--}}
-{{--        <label class="form-label inline ml-1" for="default_message_response">{{ trans('cruds.client.fields.default_message_response') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.default_message_response') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.default_message_response_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.publish_keywords_module') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="publish_keywords_module" id="publish_keywords_module" wire:model.defer="client.publish_keywords_module">--}}
-{{--        <label class="form-label inline ml-1" for="publish_keywords_module">{{ trans('cruds.client.fields.publish_keywords_module') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.publish_keywords_module') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.publish_keywords_module_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.leads_module') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="leads_module" id="leads_module" wire:model.defer="client.leads_module">--}}
-{{--        <label class="form-label inline ml-1" for="leads_module">{{ trans('cruds.client.fields.leads_module') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.leads_module') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.leads_module_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.keyword_module') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="keyword_module" id="keyword_module" wire:model.defer="client.keyword_module">--}}
-{{--        <label class="form-label inline ml-1" for="keyword_module">{{ trans('cruds.client.fields.keyword_module') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.keyword_module') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.keyword_module_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.mls_listing_module') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="mls_listing_module" id="mls_listing_module" wire:model.defer="client.mls_listing_module">--}}
-{{--        <label class="form-label inline ml-1" for="mls_listing_module">{{ trans('cruds.client.fields.mls_listing_module') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.mls_listing_module') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.mls_listing_module_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.mls_agent_notification') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="mls_agent_notification" id="mls_agent_notification" wire:model.defer="client.mls_agent_notification">--}}
-{{--        <label class="form-label inline ml-1" for="mls_agent_notification">{{ trans('cruds.client.fields.mls_agent_notification') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.mls_agent_notification') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.mls_agent_notification_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.tips_request_module') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="tips_request_module" id="tips_request_module" wire:model.defer="client.tips_request_module">--}}
-{{--        <label class="form-label inline ml-1" for="tips_request_module">{{ trans('cruds.client.fields.tips_request_module') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.tips_request_module') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.tips_request_module_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.zip_code_module') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="zip_code_module" id="zip_code_module" wire:model.defer="client.zip_code_module">--}}
-{{--        <label class="form-label inline ml-1" for="zip_code_module">{{ trans('cruds.client.fields.zip_code_module') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.zip_code_module') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.zip_code_module_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.default_zip_notification') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="default_zip_notification" id="default_zip_notification" wire:model.defer="client.default_zip_notification">--}}
-{{--        <label class="form-label inline ml-1" for="default_zip_notification">{{ trans('cruds.client.fields.default_zip_notification') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.default_zip_notification') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.default_zip_notification_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.email_address_module') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="email_address_module" id="email_address_module" wire:model.defer="client.email_address_module">--}}
-{{--        <label class="form-label inline ml-1" for="email_address_module">{{ trans('cruds.client.fields.email_address_module') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.email_address_module') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.email_address_module_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--    <div class="form-group {{ $errors->has('client.default_email_notification') ? 'invalid' : '' }}">--}}
-{{--        <input class="form-control" type="checkbox" name="default_email_notification" id="default_email_notification" wire:model.defer="client.default_email_notification">--}}
-{{--        <label class="form-label inline ml-1" for="default_email_notification">{{ trans('cruds.client.fields.default_email_notification') }}</label>--}}
-{{--        <div class="validation-message">--}}
-{{--            {{ $errors->first('client.default_email_notification') }}--}}
-{{--        </div>--}}
-{{--        <div class="help-block">--}}
-{{--            {{ trans('cruds.client.fields.default_email_notification_helper') }}--}}
-{{--        </div>--}}
-{{--    </div>--}}
+    @if(auth()->user()->is_admin)
+    <div class="form-group {{ $errors->has('client.default_messages_module') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="default_messages_module" id="default_messages_module" wire:model.defer="client.default_messages_module">
+        <label class="form-label inline ml-1" for="default_messages_module">{{ trans('cruds.client.fields.default_messages_module') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.default_messages_module') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.default_messages_module_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.default_message_notification') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="default_message_notification" id="default_message_notification" wire:model.defer="client.default_message_notification">
+        <label class="form-label inline ml-1" for="default_message_notification">{{ trans('cruds.client.fields.default_message_notification') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.default_message_notification') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.default_message_notification_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.default_message_response') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="default_message_response" id="default_message_response" wire:model.defer="client.default_message_response">
+        <label class="form-label inline ml-1" for="default_message_response">{{ trans('cruds.client.fields.default_message_response') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.default_message_response') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.default_message_response_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.publish_keywords_module') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="publish_keywords_module" id="publish_keywords_module" wire:model.defer="client.publish_keywords_module">
+        <label class="form-label inline ml-1" for="publish_keywords_module">{{ trans('cruds.client.fields.publish_keywords_module') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.publish_keywords_module') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.publish_keywords_module_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.leads_module') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="leads_module" id="leads_module" wire:model.defer="client.leads_module">
+        <label class="form-label inline ml-1" for="leads_module">{{ trans('cruds.client.fields.leads_module') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.leads_module') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.leads_module_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.keyword_module') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="keyword_module" id="keyword_module" wire:model.defer="client.keyword_module">
+        <label class="form-label inline ml-1" for="keyword_module">{{ trans('cruds.client.fields.keyword_module') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.keyword_module') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.keyword_module_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.mls_listing_module') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="mls_listing_module" id="mls_listing_module" wire:model.defer="client.mls_listing_module">
+        <label class="form-label inline ml-1" for="mls_listing_module">{{ trans('cruds.client.fields.mls_listing_module') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.mls_listing_module') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.mls_listing_module_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.mls_agent_notification') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="mls_agent_notification" id="mls_agent_notification" wire:model.defer="client.mls_agent_notification">
+        <label class="form-label inline ml-1" for="mls_agent_notification">{{ trans('cruds.client.fields.mls_agent_notification') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.mls_agent_notification') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.mls_agent_notification_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.tips_request_module') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="tips_request_module" id="tips_request_module" wire:model.defer="client.tips_request_module">
+        <label class="form-label inline ml-1" for="tips_request_module">{{ trans('cruds.client.fields.tips_request_module') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.tips_request_module') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.tips_request_module_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.zip_code_module') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="zip_code_module" id="zip_code_module" wire:model.defer="client.zip_code_module">
+        <label class="form-label inline ml-1" for="zip_code_module">{{ trans('cruds.client.fields.zip_code_module') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.zip_code_module') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.zip_code_module_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.default_zip_notification') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="default_zip_notification" id="default_zip_notification" wire:model.defer="client.default_zip_notification">
+        <label class="form-label inline ml-1" for="default_zip_notification">{{ trans('cruds.client.fields.default_zip_notification') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.default_zip_notification') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.default_zip_notification_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.email_address_module') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="email_address_module" id="email_address_module" wire:model.defer="client.email_address_module">
+        <label class="form-label inline ml-1" for="email_address_module">{{ trans('cruds.client.fields.email_address_module') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.email_address_module') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.email_address_module_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.default_email_notification') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="default_email_notification" id="default_email_notification" wire:model.defer="client.default_email_notification">
+        <label class="form-label inline ml-1" for="default_email_notification">{{ trans('cruds.client.fields.default_email_notification') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('client.default_email_notification') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.default_email_notification_helper') }}
+        </div>
+    </div>
+    <div class="form-group {{ $errors->has('client.team_id') ? 'invalid' : '' }}">
+        <label class="form-label" for="team">{{ trans('cruds.client.fields.team') }}</label>
+        <x-select-list class="form-control" id="team" name="team" :options="$this->listsForFields['team']" wire:model="client.team_id" />
+        <div class="validation-message">
+            {{ $errors->first('client.team_id') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.client.fields.team_helper') }}
+        </div>
+    </div>
+    @else
+        <input type="hidden" id="team" name="team" value="{{ auth()->user()->ownedTeam->id }}">
+    @endif
 
     <div class="form-group">
         <button class="btn btn-indigo mr-2" type="submit">
