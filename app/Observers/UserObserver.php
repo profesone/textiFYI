2f7174cb1365190b5ceb67a116b95a4d7e811d69
profesone <profesone@gmail.com>
2025-09-02 21:37:41 -0500

    Tweaked UserObserver.

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -7,20 +7,18 @@ use Illuminate\Contracts\Events\ShouldHandleEventsAfterCommit;
 
 class UserObserver implements ShouldHandleEventsAfterCommit
 {
-    public function saved(User $user): void
+    public function creating(User $user): void
     { 
         if (auth()->user()->roles === 'client') {
             $user->roles = 'client';
         }
-        if (auth()->hasUser()) {
-            // Clients can't create users
-            if (auth()->user()->roles === 'client') {
-                return;
-            }
-            if (auth()->user()->roles != 'admin') {
-                $user->agency_id = auth()->user()->agency_id;
-            }
-        }      
+        // Clients can't create users
+        if (auth()->user()->roles === 'client') {
+            return;
+        }
+        if (auth()->user()->roles != 'admin') {
+            $user->agency_id = auth()->user()->agency_id;
+        }
     }
 
     public function updating(User $user): void
