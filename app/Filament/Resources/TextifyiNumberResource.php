ff84b0503aae8f3b47d7bb4b619765e933c55122
dh_925ivy <dh_925ivy@vps40736.dreamhostps.com>
2025-09-07 13:20:41 -0700

    Added Edit button to TextiNumber Table.

--- app/Filament/Resources/TextifyiNumberResource.php
+++ app/Filament/Resources/TextifyiNumberResource.php
@@ -61,8 +61,9 @@ class TextifyiNumberResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\DeleteAction::make(),
-            ])
+		    Tables\Actions\DeleteAction::make(),
+		    Tables\Actions\EditAction::make(),
+	    ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
