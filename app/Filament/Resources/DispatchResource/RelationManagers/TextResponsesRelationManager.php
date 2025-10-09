f9487159c33e40007ae9e61b8b7e1065d8c07d4e
profesone <profesone@gmail.com>
2025-10-09 07:44:56 -0500

    MAde changes for Oct.

---
+++ app/Filament/Resources/DispatchResource/RelationManagers/TextResponsesRelationManager.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources\DispatchResource\RelationManagers;
+
+use App\Models\TextResponse;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TextResponsesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'textResponses';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('response')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('title'),
+                Forms\Components\Select::make('campagin')
+                    ->options(
+                        TextResponse::CAMPAIGN
+                    ),
+                Forms\Components\Textarea::make('notes'),
+                Forms\Components\TagsInput::make('notification_numbers'),
+                Forms\Components\TagsInput::make('keywords'),
+                Forms\Components\DatePicker::make('start_date'),
+                Forms\Components\DatePicker::make('end_date'),
+                Forms\Components\Toggle::make('active'),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('response')
+            ->columns([
+                Tables\Columns\TextColumn::make('response')
+                    ->words(10),
+                Tables\Columns\TextColumn::make('keywords')
+                    ->badge(),
+                Tables\Columns\TextColumn::make('notification_numbers')
+                    ->badge()
+                    ->label('Notification #/s'),
+                Tables\Columns\CheckboxColumn::make('active'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
