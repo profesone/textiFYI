fd9a1d4734fc99cf74ab1ca83811d8d34e45a6db
Mark Henry <profesone@gmail.com>
2025-12-16 20:41:19 -0600

    Fixed odd initial missing CSS issue.

--- app/Models/TextifyiNumber.php
+++ app/Models/TextifyiNumber.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use App\Models\Dispatch;
-use App\Models\Client;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
